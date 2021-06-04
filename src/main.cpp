e4aa1aed3c45b293bd88c1888ed435e3d03bc3be
a-zeg <>
2021-06-04 12:30:32 +0200

    deleted outdated comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,3 @@
-// I couldn't get file_dialog_open to work on macOS, so I'm using the following workaround.
-// For the code to work, there needs to exist a file meshlist.txt in the project folder.
-// This file has to contain the names of all meshes and landmark files that the user wants
-// to be able to load.
-// The name of each of these files should be in a separate line (e.g. alain_normal.obj).
-// On mac at least, this file can be easily created by running
-// ls > ../meshlist.txt
-// from the folder containing the meshes and landmark files.
-// It is assumed that this folder is named "data" (without quotes). Otherwise, the variable
-// pathtomeshes (defined below) has to be changed.
-
-// Once this file is created, it is possible to go through all the meshes in the list
-// linearly by pressing "Load next mesh" or "Load previous mesh" buttons.
-// The original meshlist.txt contains the names of all meshes and landmark files from
-// all_data/scanned_faces_cleaned
-
 #include <igl/read_triangle_mesh.h>
 #include <igl/opengl/glfw/Viewer.h>
 #include <igl/opengl/glfw/imgui/ImGuiMenu.h>
@@ -181,13 +165,12 @@ bool first_init;
 std::string pathtomeshes = "../data/"; // eg. ../data/
 
 
-// updatemeshlist reads the file specified by meshlistpath and puts all the .obj files
-// into meshlist vector and all the .landmark files into the landmarklist vector.
 void updatemeshlist () {
     meshlist.clear();
     landmarklist.clear();
 
     for (const auto& entry : filesystem::directory_iterator(pathtomeshes)) //this requires c++ 17 but the alternatives are ugly as hell :)
+    // for (const auto& entry : std::__fs::filesystem::directory_iterator(pathtomeshes)) //this requires c++ 17 but the alternatives are ugly as hell :)
     {
         std::string currentPath = entry.path().string();
         std::string file = currentPath.substr(currentPath.find_last_of("/") + 1);
@@ -304,11 +287,9 @@ int main(int argc, char* argv[])
             }
 
             ImGui::Text("Files and mesh");
-            // ToDo - Find a way to select folder and go through iteratively
-            // if (ImGui::Button("Load new mesh", ImVec2(-1, 0)))
+
             if (ImGui::Button("Load next mesh", ImVec2(-1, 0)))
             {
-                // latestMeshFileLoaded = igl::file_dialog_open();
 
                 if (current_mesh < meshlist.end()) {
                     current_mesh++;
@@ -339,7 +320,6 @@ int main(int argc, char* argv[])
             }
             if (ImGui::Button("Load previous mesh", ImVec2(-1, 0)))
             {
-                // latestMeshFileLoaded = igl::file_dialog_open();
 
                 if (current_mesh > meshlist.begin()) {
                     current_mesh--;
@@ -370,8 +350,6 @@ int main(int argc, char* argv[])
             }
             if (ImGui::Button("Load next unprocessed mesh", ImVec2(-1, 0)))
             {
-                // latestMeshFileLoaded = igl::file_dialog_open();
-                //
                 if (current_mesh < meshlist.end()) {
                     current_mesh++;
                 }
@@ -491,12 +469,10 @@ int main(int argc, char* argv[])
 
                 landmarkFileStream.close();
 
-                // cout << "Files succesfully saved to: " + fileToSaveSmoothed << " and " << fileToSaveLandmarks << endl;
                 cout << "File succesfully saved to: " << fileToSaveLandmarks << endl;
             }
             if (ImGui::Button("Save mesh and landmarks", ImVec2(-1, 0)))
             {
-                // Credits to https://stackoverflow.com/questions/4643512/replace-substring-with-another-substring-c - based on their implementation
 
                 int endingIndex = -1;
                 endingIndex = latestMeshFileLoaded.find(".obj");
@@ -750,227 +726,3 @@ void compute_handle_centroids()
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-//activate this for alternate UI (easier to debug)
-//#define UPDATE_ONLY_ON_UP
-
-using namespace std;
-using namespace Eigen;
-
-using Viewer = igl::opengl::glfw::Viewer;
-
-Viewer viewer;
-
-//vertex array, #V x3
-Eigen::MatrixXd V(0, 3);
-//face array, #F x3
-Eigen::MatrixXi F(0,3);
-
-//mouse interaction
-int down_mouse_x = -1, down_mouse_y = -1;
-
-//for selecting vertices
-std::unique_ptr<Lasso> lasso;
-
-
-
-
-
-
-
-bool callback_mouse_down(Viewer& viewer, int button, int modifier)
-{
-    if (button == (int)Viewer::MouseButton::Right)
-        return false;
-    if (V.rows() < 1)
-        return false;
-
-    down_mouse_x = viewer.current_mouse_x;
-    down_mouse_y = viewer.current_mouse_y;
-
-    if (mouse_mode == AREA)
-    {
-        if (lasso->strokeAdd(viewer.current_mouse_x, viewer.current_mouse_y) >= 0)
-            doIt = true;
-        else
-            lasso->strokeReset();
-    }
-    else
-    {
-        int vi = lasso->pickVertex(viewer.current_mouse_x, viewer.current_mouse_y);
-
-        if (vi < 0)
-            return false;
-
-        handle_id[vi] = vi;
-
-        int numFree = (handle_id.array() == -1).cast<int>().sum();
-        int num_handle_vertices = V.rows() - numFree;
-        handle_vertices.setZero(num_handle_vertices);
-        handle_vertex_positions.setZero(num_handle_vertices, 3);
-        // Quickly store which vertices are not handles for later on (simplify it)
-        not_handle_vertices.setZero(V.rows() - num_handle_vertices);
-
-        int count1 = 0;
-        int count2 = 0;
-        for (long vx = 0; vx < V.rows(); ++vx) {
-            if (handle_id[vx] >= 0) {
-                handle_vertices[count1] = vx;
-                handle_vertex_positions.row(count1++) = V.row(vx);
-            }
-            else {
-                not_handle_vertices[count2++] = vx;
-            }
-        }
-
-        // Make sure we have not saved the same vertex as a landmark before
-        for (const pair<int, int>& landmark : landmarks)
-        {
-            if (landmark.first == vi)
-                return false;
-        }
-        landmarks.push_back(std::make_pair(vi, landmarks.size() + 1));
-
-        landmark_positions.conservativeResize(landmarks.size(), 3);
-        landmark_positions.row(landmarks.size() - 1) = V.row(vi);
-
-        viewer.data().set_points(landmark_positions, Eigen::RowVector3d(1, 0, 0));
-        viewer.data().add_label(V.row(vi), to_string(landmarks.size()));
-
-    }
-
-    return true;
-}
-
-bool callback_mouse_move(Viewer& viewer, int mouse_x, int mouse_y)
-{
-    if (!doIt)
-        return false;
-    if (mouse_mode == AREA)
-    {
-        lasso->strokeAdd(mouse_x, mouse_y);
-        return true;
-    }
-    return false;
-}
-
-bool callback_mouse_up(Viewer& viewer, int button, int modifier)
-{
-    if (!doIt)
-        return false;
-    doIt = false;
-    if (mouse_mode == AREA)
-    {
-        selected_v.resize(0, 1);
-        lasso->strokeFinish(selected_v);
-        return true;
-    }
-    return false;
-};
-
-bool callback_pre_draw(Viewer& viewer)
-{
-    // initialize vertex colors
-    vertex_colors = Eigen::MatrixXd::Constant(V.rows(), 3, .9);
-
-    // first, color constraints
-    int num = handle_id.maxCoeff();
-    if (num == 0)
-        num = 1;
-    for (int i = 0; i < V.rows(); ++i)
-        if (handle_id[i] != -1)
-        {
-            int r = handle_id[i] % MAXNUMREGIONS;
-            vertex_colors.row(i) << regionColors[r][0], regionColors[r][1], regionColors[r][2];
-        }
-    // then, color selection
-    for (int i = 0; i < selected_v.size(); ++i)
-        vertex_colors.row(selected_v[i]) << 131. / 255, 131. / 255, 131. / 255.;
-
-    viewer.data().set_colors(vertex_colors);
-    viewer.data().V_material_specular.fill(0);
-    viewer.data().V_material_specular.col(3).fill(1);
-    viewer.data().dirty |= igl::opengl::MeshGL::DIRTY_DIFFUSE | igl::opengl::MeshGL::DIRTY_SPECULAR;
-
-
-    //clear points and lines
-    viewer.data().set_points(Eigen::MatrixXd::Zero(0, 3), Eigen::MatrixXd::Zero(0, 3));
-    viewer.data().set_edges(Eigen::MatrixXd::Zero(0, 3), Eigen::MatrixXi::Zero(0, 3), Eigen::MatrixXd::Zero(0, 3));
-
-    //draw the stroke of the selection
-    for (unsigned int i = 0; i < lasso->strokePoints.size(); ++i)
-    {
-        viewer.data().add_points(lasso->strokePoints[i], Eigen::RowVector3d(0.4, 0.4, 0.4));
-        if (i > 1)
-            viewer.data().add_edges(lasso->strokePoints[i - 1], lasso->strokePoints[i], Eigen::RowVector3d(0.7, 0.7, 0.7));
-    }
-
-    // update the vertex position all the time
-    viewer.data().V.resize(V.rows(), 3);
-    viewer.data().V << V;
-
-    viewer.data().dirty |= igl::opengl::MeshGL::DIRTY_POSITION;
-
-    return false;
-}
-
-bool callback_key_down(Viewer& viewer, unsigned char key, int modifiers)
-{
-  bool handled = false;
-
-  // Could add shortcuts if people want them
-
-  /*
-  if (key == 'S')
-  {
-    mouse_mode = SELECT;
-    handled = true;
-  }
-
-  if ((key == 'T') && (modifiers == IGL_MOD_ALT))
-  {
-    mouse_mode = TRANSLATE;
-    handled = true;
-  }
-
-  if ((key == 'R') && (modifiers == IGL_MOD_ALT))
-  {
-    mouse_mode = ROTATE;
-    handled = true;
-  }
-  if (key == 'A')
-  {
-    applySelection();
-    callback_key_down(viewer, '1', 0);
-    handled = true;
-  }
-
-  //viewer.ngui->refresh();
-  return handled;
-}
-*/
